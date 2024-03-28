70fe1f3300e787b613d18632cc36f03e348b172b
RBelachew <49268743+RBelachew@users.noreply.github.com>
2024-03-28 11:43:52 +0200

    adding validations to fields of the models: ClassOf, Job, Post, Event. In addition, changes of fields in models

--- server/models/classOf.js
+++ server/models/classOf.js
@@ -1,32 +1,31 @@
 const mongoose = require("mongoose");
+
 const classOfScema = new mongoose.Schema(
   {
     name: {
-      type: String,
-      required: true,
+      type: String
     },
-    Identifier:{type:String,required:[true, 'Please add an identifier'],unigue:true},
-
-    startDate: { type: Date, required: true },
-    endDate: { type: Date, required: true },
-
+    Identifier: {
+      type: String,
+      unique: true   //I fixed
+    },
+    startDate: { type: Date },
+    endDate: { type: Date },
     engagementPercentage: {
       type: Number,
-      // required: true,
       min: 0,
       max: 100,
     },
     placementPercentages: {
       type: Number,
-      // required: true,
       min: 0,
       max: 100,
     },
-
     coordinator: { type: mongoose.Types.ObjectId, ref: "Coordinators" },
     graduates: [
       {
-        type: mongoose.Types.ObjectId, ref: "Graduates",
+        type: mongoose.Types.ObjectId,
+        ref: "Graduates",
       },
     ],
   },
--- server/models/job-model.js
+++ server/models/job-model.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 const jobScema = new mongoose.Schema(
   {
-    description: { type: String, require: [true, 'Please add a description'] },
-    role: { type: String, require: [true, 'Please add a role'] },
-    company:{type:String, require: [true, 'Please add a company'] },
-    location: { type:String,  require: [true, 'Please add a location'] },
+    description: { type: String },
+    role: { type: String },
+    company:{type:String },
+    location: { type:String },
     coordinator: { type: mongoose.Types.ObjectId, ref: "Coordinators" },
     apply: [
       {
@@ -13,9 +13,9 @@ const jobScema = new mongoose.Schema(
       },
     ]
   },
-  { timestamps: true,
-
-   }
+  { 
+    timestamps: true,
+  }
 );
 
 const JobModel = mongoose.model("Jobs", jobScema);
