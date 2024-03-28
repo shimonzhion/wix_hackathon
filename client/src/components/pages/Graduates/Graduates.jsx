4ec2d42ca7d85442288e88c31e4b43da1301fa6b
DaliaZegaya <daliazegaya56@gmail.com>
2024-03-28 15:15:46 +0200

    update user table

--- client/src/components/pages/Graduates/Graduates.jsx
+++ client/src/components/pages/Graduates/Graduates.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import UserTable from "../../features/UserTable"
 function Graduates() {
   return (
     <Box sx={{ display: "flex" }}>
       <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop: "55px" }}>
         <Typography variant="h5">
-          Graduates
-          <h1 className="text-2xl font-bold underline">Hello world!</h1>
+          <UserTable/>
         </Typography>
       </Box>
     </Box>
